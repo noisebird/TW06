01e939c1ddf4d90f543f4d6e701fabd7ec2f8c64
wangjie <728199993@qq.com>
2018-04-10 13:30:48 +0800

    add practice06 and test
    Signed-off-by: wangjie <728199993@qq.com>

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,36 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private int klass;
+
+    public Teacher() {
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        String str=super.introduce();
+        if(klass!=0){
+            return str+" I am a Teacher. I teach Class "+klass+".";
+        }else{
+            return str+" I am a Teacher. I teach No Class.";
+        }
+
+    }
 }
