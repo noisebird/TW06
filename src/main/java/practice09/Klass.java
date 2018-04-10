c1e63118df2a4c82e883f444d8fec33df7447d2e
wangjie <728199993@qq.com>
2018-04-10 13:31:55 +0800

    add practice09 and test
    Signed-off-by: wangjie <728199993@qq.com>

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,50 @@
 package practice09;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Klass {
+    private  int number;
+    private Student leader;
+    private Map<Integer,Student> mapnew=new  HashMap<Integer, Student>();
+
+    public Klass() {
+    }
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public String getDisplayName(){
+        return "Class "+getNumber();
+    }
+
+    public void assignLeader(Student student){
+        if(mapnew.containsKey(student.getId())){
+            setLeader(student);
+        }else{
+            System.out.println("It is not one of us.");
+        }
+    }
+    public void appendMember(Student student) {
+       mapnew.put(student.getId(),student);
+    }
 }
