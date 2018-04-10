6dd7617f4b78043b5c827cbac2b375d9addf3e24
wangjie <728199993@qq.com>
2018-04-10 13:30:34 +0800

    add practice05 and test
    Signed-off-by: wangjie <728199993@qq.com>

--- src/main/java/practice05/Worker.java
+++ src/main/java/practice05/Worker.java
@@ -1,4 +1,16 @@
 package practice05;
 
-public class Worker {
+public class Worker extends Person {
+    public Worker() {
+    }
+
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String introduce() {
+        String str = super.introduce();
+        return str + " I am a Worker. I have a job.";
+    }
 }
