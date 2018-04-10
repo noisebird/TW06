4c943fc703179b70aae13609d94172b6f3688756
wangjie <728199993@qq.com>
2018-04-10 13:31:17 +0800

    add practice07 and test
    Signed-off-by: wangjie <728199993@qq.com>

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,29 @@
 package practice07;
 
-public class Student {
+import practice06.Person;
+
+public class Student extends Person {
+    private Klass klass;
+
+    public Student() {
+    }
+
+    public Student(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        String str=super.introduce();
+        return str+" I am a Student. I am at "+klass.getDisplayName()+".";
+    }
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,46 @@
 package practice07;
 
-public class Teacher {
+import practice06.Person;
+
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher() {
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        String str=super.introduce();
+        if(klass!=null){
+            return str+" I am a Teacher. I teach "+klass.getDisplayName()+".";
+        }else{
+            return str+" I am a Teacher. I teach No Class.";
+        }
+
+    }
+    public String introduceWith(Student student){
+        String str=super.introduce();
+        if(student.getKlass().getNumber()==klass.getNumber()){
+            return str+" I am a Teacher. I teach " + student.getName() + ".";
+        }else{
+            return str+" I am a Teacher. I don't teach " + student.getName() + ".";
+        }
+    }
 }
