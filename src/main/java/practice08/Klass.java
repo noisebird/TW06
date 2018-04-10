d034defc6bb5f92fff7c8dde6550efff6969350f
wangjie <728199993@qq.com>
2018-04-10 13:31:38 +0800

    add practice08 and test
    Signed-off-by: wangjie <728199993@qq.com>

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,37 @@
 package practice08;
 
 public class Klass {
+    private  int number;
+    private Student leader;
+
+    public Klass() {
+    }
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public String getDisplayName(){
+        return "Class "+getNumber();
+    }
+
+    public void assignLeader(Student student){
+        setLeader(student);
+    }
 }
--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,45 @@
 package practice08;
 
-public class Teacher {
+
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher() {
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        String str=super.introduce();
+        if(klass!=null){
+            return str+" I am a Teacher. I teach "+klass.getDisplayName()+".";
+        }else{
+            return str+" I am a Teacher. I teach No Class.";
+        }
+
+    }
+    public String introduceWith(Student student){
+        String str=super.introduce();
+        if(student.getKlass().getNumber()==klass.getNumber()){
+            return str+" I am a Teacher. I teach " + student.getName() + ".";
+        }else{
+            return str+" I am a Teacher. I don't teach " + student.getName() + ".";
+        }
+    }
 }
